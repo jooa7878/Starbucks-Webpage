81854689b332b0948595d0db66e04c2c9ac79a10
jooa7878 <jooa7878@naver.com>
2021-08-14 15:43:06 +0900

    added common module

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 const iconEl = searchEl.querySelector('.material-icons');
 const badgeEl = document.querySelector('header .badges');
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 const spyEls = document.querySelectorAll('section.scroll-spy');
-const thisYear = document.querySelector('.this-year');
+
 const toTopEl =document.querySelector('#to-top');
 
 let isHidePromotion = false;
-
-searchEl.addEventListener('click', () => {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
 
 window.addEventListener('scroll', _.throttle(() => {
   if (window.scrollY > 500) {
@@ -143,7 +127,7 @@ spyEls.forEach((spyEl) => {
     .addTo(new ScrollMagic.Controller()); 
 });
 
-thisYear.textContent = new Date().getFullYear();
+
 
 toTopEl.addEventListener('click', ()=>{
   gsap.to(window, .7 , {
